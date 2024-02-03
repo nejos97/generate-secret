269e20c5202853b38850d5b414a6b75fccd64c09
Jonathan Nenba <nenbajonathan@protonmail.com>
2024-02-03 15:33:45 +1300

    feat: implement generation logic, set length and maximum

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from typing import Union
 from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from string import ascii_letters
+from random import choice
 
+MAX_SECRET_LENGTH=10000
 app = FastAPI()
 
 @app.get('/')
-def generate_secret():
-    return { 'secret': 'skfjsknfkdsjfknsdkfksdnfskdfnsd' }
+def generate_secret(length: int = 32):
+    result = ''.join(choice(ascii_letters) for i in range(length))
+    if length >= MAX_SECRET_LENGTH:
+        return JSONResponse(content={ 'status': status.HTTP_400_BAD_REQUEST, 'message': f'Maximum secret length is {MAX_SECRET_LENGTH}' })
+    return JSONResponse(content={ 'secret': result.lower() })
 
 @app.get('/healthz', status_code=status.HTTP_200_OK)
 def healthz():
-    return { 'status': status.HTTP_200_OK  , 'message': 'OK' }
+    return JSONResponse(content={ 'status': status.HTTP_200_OK  , 'message': 'OK' })
