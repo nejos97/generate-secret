ff163f1abbf7d2a0a48c72531d3d0e5ac3bb4bd0
Jonathan Nenba <nenbajonathan@protonmail.com>
2024-02-03 14:55:01 +1300

    feat: add generate_secret endpoint

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('Hello world!')
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def generate_secret():
+    return { 'secret': 'skfjsknfkdsjfknsdkfksdnfskdfnsd' }
