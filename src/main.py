ac703c4a391633948d8395cc5120956ed29260cc
Jonathan Nenba <nenbajonathan@protonmail.com>
2024-02-03 15:02:43 +1300

    feat: add api health check endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 @app.get('/')
 def generate_secret():
     return { 'secret': 'skfjsknfkdsjfknsdkfksdnfskdfnsd' }
+
+@app.get('/healthz', status_code=status.HTTP_200_OK)
+def healthz():
+    return { 'status': status.HTTP_200_OK  , 'message': 'OK' }
